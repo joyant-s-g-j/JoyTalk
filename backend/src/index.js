cf28a19db4d798b22e08ae0af031b4863e1e3168
joyant-s-g-j <joyantsheikharguptajoy@gmail.com>
2026-04-17 18:51:14 +0600

    get

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,6 +26,16 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    const frontendDistPath = path.join(__dirname, "../frontend/dist");
+
+    app.use(express.static(frontendDistPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendDistPath, "index.html"));
+    });
+}
+
 server.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
     connectDB();
